452f9109f5ae399e7b23946dcd71d7bdf7b7683f
akshatp302 <akshatpanchal779@gmail.com>
2025-04-28 23:50:49 +0200

    Add classification practice file

---
+++ classification_practice.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+import numpy as np 
+import sklearn
+from sklearn.datasets import make_circles 
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+    
+    
+    
